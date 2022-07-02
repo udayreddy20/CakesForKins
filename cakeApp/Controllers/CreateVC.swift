226ce883b9ae98a441e8d26ace5bc96a2fb8fe86
udayreddy20 <75385747+udayreddy20@users.noreply.github.com>
2022-07-01 23:13:06 -0400

    createVC changes

--- cakeApp/Controllers/CreateVC.swift
+++ cakeApp/Controllers/CreateVC.swift
@@ -1,115 +1,46 @@
 //
-//  CreateVC.swift
-//  cakeApp
-
+//  SearchVC.swift
 
 
 import UIKit
-import DTPagerController
-import HMSegmentedControl
 
-
-enum TypeCake {
-    case type
-    case shape
-    case size
-    case flavour
-    case filling
-    case info
-}
-
-class model {
-    var imageString: String!
-    var title: String!
-    var typeCake: TypeCake
-    
-    init(imageString:String,title:String,typeCake:TypeCake){
-        self.imageString = imageString
-        self.title = title
-        self.typeCake = typeCake
+class SearchVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        15
     }
-}
-
-class CustomCakeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let data = data.count
-        self.vwNotes.isHidden = true
-        self.typeTbl.isHidden = false
-        if data == 0 {
-            self.vwNotes.isHidden = false
-            self.typeTbl.isHidden = true
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CakeViewCell", for: indexPath) as! CakeViewCell
+        let tap = UITapGestureRecognizer()
+        tap.addAction {
+            if let vc = UIStoryboard.main.instantiateViewController(withClass: CakeDetailsVC.self){
+                self.present(vc, animated: true) {
+                    let vc  = SuccessVC.instantiate(fromAppStoryboard: .main)
+                    self.navigationController?.pushViewController(vc, animated: true)
+                }
+            }
         }
-        return data
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TypeCell", for: indexPath) as! TypeCell
-        cell.configCell(data: data[indexPath.row])
+        cell.vwCell.isUserInteractionEnabled = true
+        cell.vwCell.addGestureRecognizer(tap)
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: ((280/375) * UIScreen.main.bounds.width), height: ((200/375) * UIScreen.main.bounds.width))
+    }
 
-    @IBOutlet weak var vwControl: UIView!
-    @IBOutlet weak var btnPrevious: UIButton!
-    @IBOutlet weak var btnContinue: UIButton!
-    @IBOutlet weak var vwNotes: UIView!
+    @IBOutlet weak var cakeList: UICollectionView!
+    @IBOutlet weak var cakeListView: UIView!
     
-    let typeData: [model] = [
-        model(imageString: "regular", title: "Regular", typeCake: .type),
-        model(imageString: "gluten free", title: "Gluten Free", typeCake: .type),
-        model(imageString: "vegan", title: "Vegan", typeCake: .type),
-        model(imageString: "eggless", title: "Eggless", typeCake: .type),
-        model(imageString: "sugar free", title: "Sugar Free", typeCake: .type),
-        model(imageString: "fondant", title: "Fodant", typeCake: .type),
-        model(imageString: "hexagon", title: "Hexagon", typeCake: .shape),
-        model(imageString: "heart", title: "Heart", typeCake: .shape),
-        model(imageString: "round", title: "Round", typeCake: .shape),
-        model(imageString: "numeric", title: "Numeric", typeCake: .shape),
-        model(imageString: "square", title: "Square", typeCake: .shape),
-        model(imageString: "alphabet", title: "Alphabet", typeCake: .shape),
-        model(imageString: "0.5 lbs", title: "Serves 2-3", typeCake: .size),
-        model(imageString: "1.0 lbs", title: "Serves 4-6", typeCake: .size),
-        model(imageString: "1.5 lbs", title: "Serves 6-8", typeCake: .size),
-        model(imageString: "2.0 lbs", title: "Serves 9-11", typeCake: .size),
-        model(imageString: "2.5 lbs", title: "Serves 12-15", typeCake: .size),
-        model(imageString: "3.0 lbs", title: "Serves 15-17", typeCake: .size),
-        model(imageString: "A", title: "Almond", typeCake: .flavour),
-        model(imageString: "B", title: "Black Current", typeCake: .flavour),
-        model(imageString: "B", title: "Butterscotch", typeCake: .flavour),
-        model(imageString: "C", title: "Chocolate", typeCake: .flavour),
-        model(imageString: "D", title: "Dark Chocolate", typeCake: .flavour),
-        model(imageString: "K", title: "Kit Kat", typeCake: .flavour),
-        model(imageString: "S", title: "Strawberry", typeCake: .filling),
-        model(imageString: "O", title: "Oreo Cookie", typeCake: .filling),
-        model(imageString: "M", title: "Mango", typeCake: .filling),
-        model(imageString: "C", title: "Chocolate Rum", typeCake: .filling),
-        model(imageString: "B", title: "Blackberry", typeCake: .filling),
-        model(imageString: "A", title: "Almonds", typeCake: .filling)
-    ]
-    
-    var data = [model]()
-    
-    let arrData = [
-        "Type",
-        "Shape",
-        "Size",
-        "Flavour",
-        "Filling",
-        "Info"
-    ]
-    
-    var segmentedControl = HMSegmentedControl()
+//    var cakeListData = [ProductModel]()
     
     
-    @IBOutlet weak var typeTbl: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setUpView()
-        self.data = self.getData(type: .type)
-        self.typeTbl.delegate = self
-        self.typeTbl.dataSource = self
-        self.btnContinue.layer.cornerRadius = 10
-        self.btnPrevious.layer.cornerRadius = 10
+//        self.getCakeList()
+        self.cakeList.delegate = self
+        self.cakeList.dataSource = self
+        self.cakeListView.layer.cornerRadius = 15
         // Do any additional setup after loading the view.
     }
     
@@ -123,138 +54,63 @@ class CustomCakeViewController: UIViewController, UITableViewDelegate, UITableVi
         self.navigationController?.navigationBar.isHidden = false
     }
 
-    
-    @IBAction func btnPrevious(_ sender: Any) {
-        if segmentedControl.selectedSegmentIndex > 0 {
-            let selectedIndex = segmentedControl.selectedSegmentIndex - 1
-            segmentedControl.selectedSegmentIndex = selectedIndex
-            switch selectedIndex {
-            case 0:
-                self.data = self.getData(type: .type)
-                self.typeTbl.reloadData()
-                return
-            case 1:
-                self.data = self.getData(type: .shape)
-                self.typeTbl.reloadData()
-                return
-            case 2:
-                self.data = self.getData(type: .size)
-                self.typeTbl.reloadData()
-                return
-            case 3:
-                self.data = self.getData(type: .flavour)
-                self.typeTbl.reloadData()
-                return
-            case 4:
-                self.data = self.getData(type: .filling)
-                self.typeTbl.reloadData()
-                return
-            case 5:
-                self.data = self.getData(type: .info)
-                self.typeTbl.reloadData()
-                return
-            default:
-                print("selectedSegmentIndex : - Error")
-        }
-        }
-    }
-    
-    @IBAction func btnContinue(_ sender: Any) {
-        if segmentedControl.selectedSegmentIndex < arrData.count - 1 {
-            let selectedIndex = segmentedControl.selectedSegmentIndex + 1
-            segmentedControl.selectedSegmentIndex = selectedIndex
-            switch selectedIndex {
-            case 1:
-                self.data = self.getData(type: .shape)
-                self.typeTbl.reloadData()
-                return
-            case 2:
-                self.data = self.getData(type: .size)
-                self.typeTbl.reloadData()
-                return
-            case 3:
-                self.data = self.getData(type: .flavour)
-                self.typeTbl.reloadData()
-                return
-            case 4:
-                self.data = self.getData(type: .filling)
-                self.typeTbl.reloadData()
-                return
-            case 5:
-                self.data = self.getData(type: .info)
-                self.typeTbl.reloadData()
-                return
-            default:
-                print("selectedSegmentIndex : - Error")
-        }
-        }
-        
-    }
-    
-    func getData(type:TypeCake) -> [model] {
-        let arrData =  self.typeData.filter { model in
-           if model.typeCake == type {
-                return true
-            }
-            return false
-        }
-        return arrData
-    }
-    
-    func setUpView(){
-        segmentedControl = HMSegmentedControl(sectionTitles: arrData)
-        segmentedControl.backgroundColor = UIColor.clear
-        segmentedControl.selectionIndicatorColor = .clear
-        segmentedControl.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white];
-        segmentedControl.selectedTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white.cgColor];
-        
-        segmentedControl.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width == 375.0 ? ( self.vwControl.layer.frame.width-30): (self.vwControl.layer.frame.width), height: self.vwControl.frame.height)
-        segmentedControl.addTarget(self, action: #selector(self.getValue(_:)), for: .valueChanged)
-        vwControl.addSubview(segmentedControl)
-    }
-    
-    @objc func getValue(_ sender: HMSegmentedControl){
-        print(self.arrData[Int(exactly: sender.selectedSegmentIndex)!].description)
-        switch sender.selectedSegmentIndex {
-        case 0:
-            self.data = self.getData(type: .type)
-            self.typeTbl.reloadData()
-            return
-        case 1:
-            self.data = self.getData(type: .shape)
-            self.typeTbl.reloadData()
-            return
-        case 2:
-            self.data = self.getData(type: .size)
-            self.typeTbl.reloadData()
-            return
-        case 3:
-            self.data = self.getData(type: .flavour)
-            self.typeTbl.reloadData()
-            return
-        case 4:
-            self.data = self.getData(type: .filling)
-            self.typeTbl.reloadData()
-            return
-        case 5:
-            self.data = self.getData(type: .info)
-            self.typeTbl.reloadData()
-            return
-        default:
-            print("selectedSegmentIndex : - Error")
-        }
-    }
-    
-
 }
 
-class TypeCell: UITableViewCell {
+
+
+class CakeViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var lblTitle: UILabel!
-    @IBOutlet weak var imgCakeType: UIImageView!
     
-    func configCell(data: model) {
-        self.imgCakeType.setImageForName(string: data.imageString ?? "", circular: true)
-        self.lblTitle.text = data.title
+    @IBOutlet weak var vwCell: UIView!
+    @IBOutlet weak var widthConst: NSLayoutConstraint!
+    @IBOutlet weak var imgCake: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        if UIScreen.main.bounds.width == 375 {
+            self.widthConst.constant = 280
+        }else{
+            self.widthConst.constant = 320
+        }
+        self.imgCake.layer.cornerRadius = 10.0
     }
+    
+//    func configCell(data: ProductModel) {
+//        self.lblName.text = data.name
+//        self.imgCake.setImgWebUrl(url: data.image, isIndicator: true)
+//    }
+//
+//    func configCellFav(data: FavouriteModel) {
+//        self.lblName.text = data.name
+//        self.imgCake.setImgWebUrl(url: data.image, isIndicator: true)
+//    }
 }
+
+
+//extension SearchVC {
+//    func getCakeList() {
+//        _ = AppDelegate.shared.db.collection(cProduct).addSnapshotListener{ querySnapshot, error in
+//            guard let snapshot = querySnapshot else {
+//                print("Error fetching snapshots: \(error!)")
+//                return
+//            }
+//            self.cakeListData.removeAll()
+//            if snapshot.documents.count != 0 {
+//                for data in snapshot.documents{
+//                    let data = data.data()
+//                    print(data)
+//                    self.cakeListData.append(ProductModel(id: data["id"] as! String, image: (data["image"] as! String), name: (data["name"] as! String), description: (data["description"] as! String), price: (data["price"] as! String)))
+//                }
+//                self.cakeList.delegate = self
+//                self.cakeList.dataSource = self
+//                self.cakeList.reloadData()
+//
+//            }else{
+//                Alert.shared.showAlert(message: "No Data Found !!!", completion: nil)
+//            }
+//        }
+//
+//    }
+//}
+//
