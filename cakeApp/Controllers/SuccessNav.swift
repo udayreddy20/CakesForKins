8a597ec2a3b69fd9c8c1bca45a9abb3664352d41
udayreddy20 <75385747+udayreddy20@users.noreply.github.com>
2022-07-05 23:06:28 -0300

    Create SuccessNav.swift

---
+++ cakeApp/Controllers/SuccessNav.swift
@@ -0,0 +1,15 @@
+//
+//  SuccessNav.swift
+//  cakeApp
+
+import UIKit
+
+class SuccessNav: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
